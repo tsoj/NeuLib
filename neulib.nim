c19b311b54d7523bb3e3ccd6a586295ab3cc7d71
tsoj <tsoj.tsoj@gmx.de>
2022-05-22 00:12:35 +0200

    Wrote more code

--- neulib.nim
+++ neulib.nim
@@ -1,8 +1,8 @@
 type
     Float = float32
     ActivationFunction = object
-        f: proc(x: Float): Float
-        df: proc(x: Float): Float
+        f: proc(x: Float): Float {.noSideEffect.}
+        df: proc(x: Float): Float {.noSideEffect.}
     Layer = object
         bias: seq[Float]
         weights: seq[Float]
@@ -12,8 +12,14 @@ type
     Network* = object
         layers: seq[Layer]
     Nothing = enum nothing
+    LayerBackpropInfo = object
+        layer: Layer
+        preActivation: seq[Float]
+        postActivation: seq[Float]
+        inputGradient: seq[Float]
     BackpropInfo = object
-        discard # TODO
+        layers: seq[LayerBackpropInfo]
+        input: seq[Float]
 
 func getBackpropInfo*(network: Network): BackpropInfo =
     discard # TODO
@@ -30,6 +36,8 @@ func newLayer*(numInputs, numOutputs: int, activation: ActivationFunction): Laye
     )
 
 func newNetwork*(description: varargs[tuple[numNeurons: int, activation: ActivationFunction]]): Network =
+    assert description.len >= 2, "Network needs at least one input layer and one output layer"
+
     for i in 1..<description.len:
         result.layers.add(newLayer(
             numInputs = description[i-1].numNeurons,
@@ -43,41 +51,119 @@ func weightIndex(inNeuron, outNeuron, numInputs, numOutputs: int): int =
 
     outNeuron + numOutputs * inNeuron;
 
+func forward(
+    layer: Layer,
+    input: openArray[Float],
+    layerBackpropInfo: Nothing or var LayerBackpropInfo
+): seq[Float] =
+    result = layer.bias
+    
+    for inNeuron in 0..<layer.numInputs:
+        for outNeuron in 0..<layer.numOutputs:
+            let i = weightIndex(inNeuron, outNeuron, layer.numInputs, layer.numOutputs)
+            result[outNeuron] += layer.weights[i] * input[inNeuron]
+
+    when layerBackpropInfo isnot Nothing:
+        layerBackpropInfo.preActivation = result
+
+    for value in result.mitems:
+        value = layer.activation.f(value)
+
+    when layerBackpropInfo isnot Nothing:
+        layerBackpropInfo.postActivation = result
+
+func backward(
+    layer: Layer,
+    outGradient: openArray[Float],
+    inPostActivation: openArray[Float],
+    layerBackpropInfo: var LayerBackpropInfo
+) =
+
+    assert layerBackpropInfo.layer.numOutputs == layer.numOutputs
+    assert layerBackpropInfo.layer.bias.len == layer.numOutputs
+    assert layerBackpropInfo.layer.numInputs == layer.numInputs
+    assert layerBackpropInfo.layer.weights.len == layer.numInputs * layer.numOutputs
+    assert outGradient.len == layer.numOutputs
+    assert inPostActivation.len == layer.numInputs
+    assert layerBackpropInfo.inputGradient.len == layer.numInputs
+    assert(block:
+        var b = true
+        for inNeuron in 0..<layer.numInputs:
+            if layerBackpropInfo.inputGradient[inNeuron] != 0:
+                b = false
+        b
+    )
+
+    for outNeuron in 0..<layer.numOutputs:
+        layerBackpropInfo.layer.bias[outNeuron] =
+            layer.activation.df(layerBackpropInfo.preActivation[outNeuron]) * outGradient[outNeuron]
+
+    for inNeuron in 0..<layer.numInputs:
+        for outNeuron in 0..<layer.numOutputs:
+            let i = weightIndex(inNeuron, outNeuron, layer.numInputs, layer.numOutputs)
+            layerBackpropInfo.layer.weights[i] += inPostActivation[inNeuron] * layerBackpropInfo.layer.bias[outNeuron]
+            layerBackpropInfo.inputGradient[inNeuron] += layer.weights[i] * layerBackpropInfo.layer.bias[outNeuron]
 
 func forward(
     network: Network,
     input: openArray[Float],
-    output: var openArray[Float],
     backpropInfo: Nothing or var BackpropInfo
-) =
-    assert network.layers.len >= 2, "Network needs at least one input layer and one output layer"
+): seq[Float] =
+
+    assert network.layers.len >= 1, "Network needs at least one input layer and one output layer"
     assert input.len == network.layers[0].numInputs, "Input size and input size of first layer must be the same"
-    assert output.len == network.layers[^1].numOutputs, "Output size and output size of last layer must be the same"
+    assert result.len == network.layers[^1].numOutputs, "Output size and output size of last layer must be the same"
+    when backpropInfo isnot Nothing:
+        assert backpropInfo.layers.len == network.layers.len
+
+    result = forward(
+        network.layers[0],
+        input,
+        when backpropInfo is Nothing: nothing else: backpropInfo.layers[0]
+    )
+
+    when backpropInfo isnot Nothing:
+        backpropInfo.input = input.toSeq
+
+    for i in 1..<network.layers.len:
+        result = forward(
+            network.layers[i],
+            result,
+            when backpropInfo is Nothing: nothing else: backpropInfo.layers[i]
+        )
 
 func forward*(
     network: Network,
     input: openArray[Float],
-    output: var openArray[Float],
     backpropInfo: var BackpropInfo
-) =
-    forward(network, input, output, backpropInfo)
-
-func forward*(
-    network: Network,
-    input: openArray[Float],
-    output: var openArray[Float]
-) =
-    network.forward(input, output, nothing)
+): seq[Float] =
+    network.forward(input, backpropInfo)
 
 func forward*(
     network: Network,
     input: openArray[Float]
 ): seq[Float] =
-    result = newSeq[Float](network.layers[^1].numOutputs)
-    network.forward(input, result)
+    network.forward(input, nothing)
 
+func backward*(
+    network: Network,
+    lossGradient: openArray[Float],
+    backpropInfo: var BackpropInfo
+) =
 
+    assert network.layers.len >= 1, "Network needs at least one input layer and one output layer"
+    assert lossGradient.len == network.layers[^1].numOutputs, "Loss size and output size of last layer must be the same"
+    assert backpropInfo.layers.len == network.layers.len
 
+    for i in countdown(network.layers.len - 1, 0):
+
+        backward(
+            network.layers[i],
+            if i == network.layers.len - 1: lossGradient else: backpropInfo.layers[i + 1].inputGradient,
+            if i == 0: backpropInfo.input else: backpropInfo.layers[i - 1].postActivation,
+            backpropInfo.layers[i]
+        )
+    
 
 
 
