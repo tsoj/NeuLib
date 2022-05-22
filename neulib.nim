05a77eb17b80244bb57059011a7f83a252c963d7
tsoj <tsoj.tsoj@gmx.de>
2022-05-22 23:31:20 +0200

    Fixed some bugs

--- neulib.nim
+++ neulib.nim
@@ -1,8 +1,13 @@
+import
+    math,
+    sequtils
+
 type
     Float = float32
     ActivationFunction = object
         f: proc(x: Float): Float {.noSideEffect.}
         df: proc(x: Float): Float {.noSideEffect.}
+        name: string
     Layer = object
         bias: seq[Float]
         weights: seq[Float]
@@ -11,18 +16,50 @@ type
         activation: ActivationFunction
     Network* = object
         layers: seq[Layer]
-    Nothing = enum nothing
+    Nothing = enum a
     LayerBackpropInfo = object
-        layer: Layer
+        paramGradient: Layer
         preActivation: seq[Float]
         postActivation: seq[Float]
         inputGradient: seq[Float]
     BackpropInfo = object
         layers: seq[LayerBackpropInfo]
         input: seq[Float]
+        numSummedGradients: int
+
+func setZero(s: var seq[Float]) =
+    for v in s.mitems:
+        v = 0
+
+func setZero*(backpropInfo: var BackpropInfo) =
+    for layerBackpropInfo in backpropInfo.layers.mitems:
+        layerBackpropInfo.paramGradient.bias.setZero
+        layerBackpropInfo.paramGradient.weights.setZero
+        layerBackpropInfo.inputGradient.setZero
+    backpropInfo.numSummedGradients = 0
 
 func getBackpropInfo*(network: Network): BackpropInfo =
-    discard # TODO
+    for layer in network.layers:
+        result.layers.add(LayerBackpropInfo(
+            paramGradient: layer,
+            inputGradient: newSeq[Float](layer.numInputs)
+        ))
+    result.setZero
+
+func addGradient*(network: var Network, backpropInfo: BackpropInfo, lr: Float) =
+    assert network.layers.len == backpropInfo.layers.len
+
+    for layerIndex in 0..<network.layers.len:
+        assert network.layers[layerIndex].bias.len == backpropInfo.layers[layerIndex].paramGradient.bias.len
+        assert network.layers[layerIndex].weights.len == backpropInfo.layers[layerIndex].paramGradient.weights.len
+        
+        for i in 0..<network.layers[layerIndex].bias.len:
+            network.layers[layerIndex].bias[i] +=
+                (lr * backpropInfo.layers[layerIndex].paramGradient.bias[i]) / backpropInfo.numSummedGradients.Float
+        
+        for i in 0..<network.layers[layerIndex].weights.len:
+            network.layers[layerIndex].weights[i] +=
+                (lr * backpropInfo.layers[layerIndex].paramGradient.weights[i]) / backpropInfo.numSummedGradients.Float
 
 func newLayer*(numInputs, numOutputs: int, activation: ActivationFunction): Layer =
     assert numInputs > 0 and numOutputs > 0
@@ -35,15 +72,22 @@ func newLayer*(numInputs, numOutputs: int, activation: ActivationFunction): Laye
         activation: activation
     )
 
-func newNetwork*(description: varargs[tuple[numNeurons: int, activation: ActivationFunction]]): Network =
-    assert description.len >= 2, "Network needs at least one input layer and one output layer"
+func newNetwork*(numInputs: int, layers: varargs[tuple[numNeurons: int, activation: ActivationFunction]]): Network =
+    assert layers.len >= 2, "Network needs at least one input layer and one output layer"
 
-    for i in 1..<description.len:
+    for i in 0..<layers.len:
         result.layers.add(newLayer(
-            numInputs = description[i-1].numNeurons,
-            numOutputs = description[i].numNeurons,
-            activation = description[i-1].activation
+            numInputs = if i == 0: numInputs else: layers[i-1].numNeurons,
+            numOutputs = layers[i].numNeurons,
+            activation = layers[i].activation
         ))
+
+func `$`*(network: Network): string =
+    if network.layers.len == 0:
+        return
+    result &= $network.layers[0].numInputs
+    for layer in network.layers:
+        result &= "\n" & $layer.numOutputs & " " & layer.activation.name
 
 func weightIndex(inNeuron, outNeuron, numInputs, numOutputs: int): int =
     assert inNeuron in 0..<numInputs
@@ -54,7 +98,7 @@ func weightIndex(inNeuron, outNeuron, numInputs, numOutputs: int): int =
 func forward(
     layer: Layer,
     input: openArray[Float],
-    layerBackpropInfo: Nothing or var LayerBackpropInfo
+    layerBackpropInfo: var (Nothing or LayerBackpropInfo)
 ): seq[Float] =
     result = layer.bias
     
@@ -79,10 +123,10 @@ func backward(
     layerBackpropInfo: var LayerBackpropInfo
 ) =
 
-    assert layerBackpropInfo.layer.numOutputs == layer.numOutputs
-    assert layerBackpropInfo.layer.bias.len == layer.numOutputs
-    assert layerBackpropInfo.layer.numInputs == layer.numInputs
-    assert layerBackpropInfo.layer.weights.len == layer.numInputs * layer.numOutputs
+    assert layerBackpropInfo.paramGradient.numOutputs == layer.numOutputs
+    assert layerBackpropInfo.paramGradient.bias.len == layer.numOutputs
+    assert layerBackpropInfo.paramGradient.numInputs == layer.numInputs
+    assert layerBackpropInfo.paramGradient.weights.len == layer.numInputs * layer.numOutputs
     assert outGradient.len == layer.numOutputs
     assert inPostActivation.len == layer.numInputs
     assert layerBackpropInfo.inputGradient.len == layer.numInputs
@@ -95,41 +139,40 @@ func backward(
     )
 
     for outNeuron in 0..<layer.numOutputs:
-        layerBackpropInfo.layer.bias[outNeuron] =
+        layerBackpropInfo.paramGradient.bias[outNeuron] =
             layer.activation.df(layerBackpropInfo.preActivation[outNeuron]) * outGradient[outNeuron]
 
     for inNeuron in 0..<layer.numInputs:
         for outNeuron in 0..<layer.numOutputs:
             let i = weightIndex(inNeuron, outNeuron, layer.numInputs, layer.numOutputs)
-            layerBackpropInfo.layer.weights[i] += inPostActivation[inNeuron] * layerBackpropInfo.layer.bias[outNeuron]
-            layerBackpropInfo.inputGradient[inNeuron] += layer.weights[i] * layerBackpropInfo.layer.bias[outNeuron]
+            layerBackpropInfo.paramGradient.weights[i] +=
+                inPostActivation[inNeuron] * layerBackpropInfo.paramGradient.bias[outNeuron]
+            layerBackpropInfo.inputGradient[inNeuron] +=
+                layer.weights[i] * layerBackpropInfo.paramGradient.bias[outNeuron]
 
-func forward(
+func forwardInternal(
     network: Network,
     input: openArray[Float],
-    backpropInfo: Nothing or var BackpropInfo
+    backpropInfo: var (BackpropInfo or Nothing)
 ): seq[Float] =
+
+    result = newSeq[Float](network.layers[^1].numOutputs)
 
     assert network.layers.len >= 1, "Network needs at least one input layer and one output layer"
     assert input.len == network.layers[0].numInputs, "Input size and input size of first layer must be the same"
     assert result.len == network.layers[^1].numOutputs, "Output size and output size of last layer must be the same"
+
     when backpropInfo isnot Nothing:
         assert backpropInfo.layers.len == network.layers.len
 
-    result = forward(
-        network.layers[0],
-        input,
-        when backpropInfo is Nothing: nothing else: backpropInfo.layers[0]
-    )
-
-    when backpropInfo isnot Nothing:
         backpropInfo.input = input.toSeq
 
-    for i in 1..<network.layers.len:
+    for i in 0..<network.layers.len:
+        var nothing: Nothing
         result = forward(
-            network.layers[i],
-            result,
-            when backpropInfo is Nothing: nothing else: backpropInfo.layers[i]
+            layer = network.layers[i],
+            input = if i == 0: input else: result,
+            layerBackpropInfo = when backpropInfo is Nothing: nothing else: backpropInfo.layers[i]
         )
 
 func forward*(
@@ -137,13 +180,14 @@ func forward*(
     input: openArray[Float],
     backpropInfo: var BackpropInfo
 ): seq[Float] =
-    network.forward(input, backpropInfo)
+    network.forwardInternal(input, backpropInfo)
 
 func forward*(
     network: Network,
     input: openArray[Float]
 ): seq[Float] =
-    network.forward(input, nothing)
+    var nothing: Nothing
+    network.forwardInternal(input, nothing)
 
 func backward*(
     network: Network,
@@ -156,15 +200,90 @@ func backward*(
     assert backpropInfo.layers.len == network.layers.len
 
     for i in countdown(network.layers.len - 1, 0):
-
         backward(
-            network.layers[i],
-            if i == network.layers.len - 1: lossGradient else: backpropInfo.layers[i + 1].inputGradient,
-            if i == 0: backpropInfo.input else: backpropInfo.layers[i - 1].postActivation,
-            backpropInfo.layers[i]
+            layer = network.layers[i],
+            outGradient = if i == network.layers.len - 1: lossGradient else: backpropInfo.layers[i + 1].inputGradient,
+            inPostActivation = if i == 0: backpropInfo.input else: backpropInfo.layers[i - 1].postActivation,
+            layerBackpropInfo = backpropInfo.layers[i]
         )
+
+    backpropInfo.numSummedGradients += 1
     
 
+#[-----------------Activation Functions-----------------]#
 
 
-#var model = newNetwork((1000, relu), (500, relu), (600, relu), (1, sigmoid))
+const sigmoid* = ActivationFunction(
+    f: proc(x: Float): Float = 1.Float / (1.Float + exp(-x)),
+    df: proc(x: Float): Float =
+        let t = 1.Float / (1.Float + exp(-x))
+        t * (1.Float - t),
+    name: "sigmoid"
+)
+
+const softplus* = ActivationFunction(
+    f: proc(x: Float): Float = ln(1.Float + exp(x)),
+    df: proc(x: Float): Float = 1.Float / (1.Float + exp(-x)),
+    name: "softplus"
+)
+
+const silu* = ActivationFunction(
+    f: proc(x: Float): Float = x / (1.Float + exp(-x)),
+    df: proc(x: Float): Float = (1.Float + exp(-x) + x * exp(-x)) / pow(1.Float + exp(-x), 2.Float),
+    name: "silu"
+)
+
+const relu* = ActivationFunction(
+    f: proc(x: Float): Float = (if x > 0: x else: 0.Float),
+    df: proc(x: Float): Float = (if x > 0: 1.Float else: 0.Float),
+    name: "relu"
+)
+
+const elu* = ActivationFunction(
+    f: proc(x: Float): Float = (if x > 0: x else: exp(x) - 1.Float),
+    df: proc(x: Float): Float = (if x > 0: 1.Float else: exp(x)),
+    name: "elu"
+)
+
+const leakyRelu* = ActivationFunction(
+    f: proc(x: Float): Float = (if x > 0: x else: 0.01.Float * x),
+    df: proc(x: Float): Float = (if x > 0: 1.Float else: 0.01.Float),
+    name: "leakyRelu"
+)
+
+const identity* = ActivationFunction(
+    f: proc(x: Float): Float = x,
+    df: proc(x: Float): Float = 1.Float,
+    name: "identity"
+)
+
+const tanh* = ActivationFunction(
+    f: proc(x: Float): Float = tanh(x),
+    df: proc(x: Float): Float = 1.Float - pow(tanh(x), 2.Float),
+    name: "tanh"
+)
+
+func mseGradient*(
+    target: openArray[Float],
+    output: openArray[Float]
+): seq[Float] =
+    result = newSeq[Float](target.len)
+
+    assert target.len == output.len
+    assert output.len == result.len
+
+    for i in 0..<target.len:
+        result[i] = 2.Float * (output[i] - target[i])
+
+
+
+var model = newNetwork(1000, (500, relu), (600, relu), (1, sigmoid))
+echo model
+
+var
+    input = newSeq[Float](1000)
+    backpropInfo = model.getBackpropInfo
+
+let x = model.forward(input, backpropInfo)
+model.backward(x.mseGradient(@[4.0'f32]), backpropInfo)
+#echo backpropInfo
