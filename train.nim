54da72b8469bde51ba810f8655f3fcda17f66706
tsoj <tsoj.tsoj@gmx.de>
2022-06-15 03:22:14 +0200

    Changed MNIST example a bit, make demo of sparse inputs a bit clearer

--- train.nim
+++ train.nim
@@ -30,6 +30,7 @@ echo "Beginning with training ..."
 
 for epoch in 0..<10:
     let start = now()
+    let usingSparseInputs = epoch mod 2 == 0
 
     var shuffledIndices = (0..<trainX.len).toSeq
     shuffledIndices.shuffle
@@ -42,19 +43,22 @@ for epoch in 0..<10:
             let index = shuffledIndices[batch * batchSize + i]
 
             var output: seq[Float]
-            if rand(1.0) > 0.5:
-                # normal input layout
-                output = model.forward(trainX[index], backpropInfo)
-            else:
+            if usingSparseInputs:
                 # try out sparse input layout
                 output = model.forward(trainX[index].toSparse, backpropInfo)
+            else:
+                # normal input layout
+                output = model.forward(trainX[index], backpropInfo)
 
             let lossGradient = mseGradient(target = trainY[index], output = output)
 
             model.backward(lossGradient, backpropInfo)
         model.addGradient(backpropInfo, lr)
 
-    echo "Finished epoch ", epoch, " in ", (now() - start).inMilliseconds, " ms"
+    echo(
+        "Finished epoch ", epoch, " in ", (now() - start).inMilliseconds, " ms (used ",
+        if usingSparseInputs: "sparse" else: "full", " inputs)"
+    )
 
 
     var numCorrect = 0
